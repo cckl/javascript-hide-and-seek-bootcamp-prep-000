315ab9b25545e8ac86ed4ecba23f669288631c66
Christina L <clingc7@gmail.com>
2018-07-30 01:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function increaseRankBy(n) {
 function deepestChild () {
   // return document.querySelector('#grand-node div div div div')
   
-  const list = document.querySelectorAll('#grand-node div')
+  const list = document.querySelectorAll('div#grand-node')
+  return list.length
   const lastItem = list.length-1
   return list[lastItem]
 }
