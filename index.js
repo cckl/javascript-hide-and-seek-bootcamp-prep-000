000b5f374eb94ea8f2bfc57179be961cbbd897f6
Christina L <clingc7@gmail.com>
2018-07-30 01:39:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('div div')
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < list.length; i++) {
+    let listItem = list[i];
+    listItem.innerHTML = (i + n)
+    // this worked both when i used parseInt and when i didn't
+  }
+}
+
+function deepestChild () {
+  // return document.querySelector('#grand-node div div div div')
+  
+  const list = document.querySelectorAll('#grand-node')
+  for (let i = 0; i < list.length; i++) {
+    return list[i]
+  }
+}
